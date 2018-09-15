309c9fb9022faa4f04b5dfd94b002080b5f441f5
reddy <reddy@ramana-PC>
2018-09-15 10:53:30 +0530

    hhhh

---
+++ prt/src/prt/Example.java
@@ -0,0 +1,12 @@
+package prt;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hariiiii"); 
+//////////////////////
+////
+	}
+
+}
---
+++ prt/src/prt/prt1.java
@@ -0,0 +1,10 @@
+package prt;
+
+public class prt1 {
+
+	public static void main(String[] args) {
+		 ///////
+		//////new
+	}///neeee
+
+}
